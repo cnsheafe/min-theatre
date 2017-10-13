9c24862e48bd9a997afa4aab970f1259386178c7
cnsheafe <cnsheafe@gmail.com>
2017-10-13 15:33:13 -0600

    search bar template

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <form action="">
+        <label for="search-input">Search Youtube Videos</label>
+        <input type="text" id="search-input" formaction="submit">
+        <button type="submit">Search</button>
+    </form>
+</template>
+
+<script>
+export default {
+  name: "SearchBar"
+}
+</script>
