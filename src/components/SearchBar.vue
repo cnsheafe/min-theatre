4ca7987bffe7217b265552d25db5efcf4bdff401
cnsheafe <cnsheafe@gmail.com>
2017-10-16 11:31:13 -0600

    added on-handler template

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,9 +1,9 @@
 <template>
-    <form action="">
-        <label for="search-input">Search Youtube Videos</label>
-        <input type="text" id="search-input" formaction="submit">
-        <button type="submit">Search</button>
-    </form>
+  <form action="" v-on="fetchSearchResults">
+    <label for="search-input">Search Youtube Videos</label>
+    <input type="text" id="search-input" formaction="submit">
+    <button type="submit">Search</button>
+  </form>
 </template>
 
 <script>
