612ce2575b733e22a17691b68253c16fa12b7042
cnsheafe <cnsheafe@gmail.com>
2017-10-17 16:02:57 -0500

    connect store to view

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,17 @@
 <template>
     <section>
         <search-bar></search-bar>
+        <search-results></search-results>
     </section>
 </template>
 
 <script>
+import store from "../store";
 import SearchBar from "./SearchBar";
+import SearchResults from "./SearchResults";
 export default {
   name: "app",
-  components: {SearchBar: SearchBar}
+  components: {SearchBar: SearchBar, SearchResults: SearchResults},
+  store: store
 }
 </script>
