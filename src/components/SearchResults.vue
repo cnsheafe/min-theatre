f97c5221279220892c025dfada87af045cc0d578
cnsheafe <cnsheafe@gmail.com>
2017-10-17 16:03:34 -0500

    view for search results

---
+++ src/components/SearchResults.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <ul>
+    <li v-for="(results, index) in searchResults" :key="index">{{results}}</li>
+  </ul>
+</div>
+</template>
+
+<script>
+import {mapState} from "vuex";
+
+export default {
+  name: "results",
+  computed: mapState(['searchResults']),
+  mounted() {
+      console.log(this.searchResults);
+  }
+}
+</script>
+
