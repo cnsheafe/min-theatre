6fc39c15e5a73a250ea2b7e4a4995d75d7dff380
cnsheafe <cnsheafe@gmail.com>
2017-10-18 13:05:38 -0600

    moved mutations and actions into separate modules

--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
 import Vuex from "vuex";
 import Vue from "vue";
-Vue.use(Vuex);
 
+import {fetchSearchResults, updateSearchResults} from "./model/search";
+
+
+Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
@@ -9,26 +12,10 @@ const store = new Vuex.Store({
         currentVideo: {}
     },
     mutations: {
-        fetchSearchResults(state, results) {
-            state.searchResults = results.map(result => {
-                return result.id.videoId;
-            });
-        }
+        updateSearchResults
     },
     actions: {
-        fetchSearchResults(context, query) {
-            const searchRequest = new Request(
-                `https://min-theatre-api.herokuapp.com/search?q=hello`, {
-                    method: "GET"
-                }
-            );
-            fetch(searchRequest)
-                .then(res => {
-                    return res.json();
-                }).then(json => {
-                    context.commit("fetchSearchResults", json.items);
-                });
-        }
+        fetchSearchResults
     }
 });
 
