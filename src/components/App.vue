72ee5447e212ee29eff21cee9663cc9264bf78cd
cnsheafe <cnsheafe@gmail.com>
2017-10-19 17:32:27 -0600

    added player and formatting

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,6 +2,7 @@
     <section>
         <search-bar></search-bar>
         <search-results></search-results>
+        <video-player></video-player>
     </section>
 </template>
 
@@ -9,9 +10,15 @@
 import store from "../store";
 import SearchBar from "./SearchBar";
 import SearchResults from "./SearchResults";
+import VideoPlayer from "./VideoPlayer";
+
 export default {
   name: "app",
-  components: {SearchBar: SearchBar, SearchResults: SearchResults},
+  components: {
+    SearchBar: SearchBar,
+    SearchResults: SearchResults,
+    VideoPlayer: VideoPlayer
+  },
   store: store
-}
+};
 </script>
