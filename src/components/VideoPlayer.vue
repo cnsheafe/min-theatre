c8fecfee654f1cde449f925bc5e6d93bd9f7c9e3
cnsheafe <cnsheafe@gmail.com>
2017-10-19 17:27:38 -0600

    vue for videoplayer

---
+++ src/components/VideoPlayer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="player"></div>
+</template>
+
+<script>
+import store from "../store";
+import {mapState} from "vuex";
+
+export default {
+  name: "VideoPlayer",
+  computed: mapState(['currentVideo'])
+}
+</script>
+
