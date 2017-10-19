0dcd42f59a17f56d8773fab7da6aa69f25b39a72
cnsheafe <cnsheafe@gmail.com>
2017-10-19 17:30:53 -0600

    added actions and mutations for results and video functionality

--- src/store.js
+++ src/store.js
@@ -1,22 +1,37 @@
 import Vuex from "vuex";
 import Vue from "vue";
 
-import {fetchSearchResults, updateSearchResults} from "./model/search";
-
+import {
+  fetchSearchResults,
+  fetchResultInfo,
+  updateSearchResults,
+  addExtraInfo
+} from "./model/search";
+import {
+  loadVideo,
+  showNewVideo
+} from "./model/video";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-    state: {
-        searchResults: [],
-        currentVideo: {}
-    },
-    mutations: {
-        updateSearchResults
-    },
-    actions: {
-        fetchSearchResults
+  state: {
+    searchResults: [],
+    currentVideo: {
+      show: false,
+      id: null
     }
+  },
+  mutations: {
+    updateSearchResults,
+    addExtraInfo,
+    showNewVideo
+  },
+  actions: {
+    fetchSearchResults,
+    fetchResultInfo,
+    loadVideo
+  }
 });
 
 export default store;
