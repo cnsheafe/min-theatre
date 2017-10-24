a9dbc0b953d6140ed9fe8e4e1448d84a0a45e7c8
cnsheafe <cnsheafe@gmail.com>
2017-10-24 11:09:43 -0600

    added flexbox to result list items

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -1,19 +1,19 @@
 <template>
-<div v-show="!currentVideo.show">
+<div v-show="!currentVideo.show" class="search-results">
   <ul>
     <li v-for="(result, index) in searchResults" :key="index">
       <div class="result-left">
-        <div class="result-row">
+        <div class="result-header">
           <p @click="loadVideo" :data-index="index">{{result.title}}</p>
           <p>{{result.channelName}}</p>
         </div>
         <div class="result-row">
           <p>{{result.description}}</p>
         </div>
-        <div class="result-row">
+        <div class="result-stats">
           <div class="likes">{{result.likes}}</div>
           <div class="dislikes">{{result.dislikes}}</div>
-          <div class="views">{{result.views}}</div>
+          <!-- <div class="views">{{result.views}}</div> -->
           <div class="duration">{{result.duration}}</div>
         </div>
       </div>
@@ -41,3 +41,47 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.search-results {
+  width: 80%;
+  margin: auto;
+  font-family: "Oswald", sans-serif;
+}
+
+ul {
+  padding-left: 0;
+  list-style: none;
+}
+
+li {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 20px;
+}
+
+p {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.result-header {
+  p {
+    margin-top: 0;
+    margin-bottom: 10px;
+  }
+  p:first-child {
+    font-size: 20px;
+    text-decoration: underline;
+  }
+  p:last-child {
+    font-size: 16px;
+  }
+}
+.result-stats {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+</style>
