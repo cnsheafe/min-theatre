3f5966aa962619066b84d8c9bce7c05c9d8ada31
cnsheafe <cnsheafe@gmail.com>
2017-10-23 21:14:10 -0600

    added actions and mutations to clear video

--- src/model/video.js
+++ src/model/video.js
@@ -1,19 +1,35 @@
-export function loadVideo(context, videoId, playerWidth) {
-  let player = new YT.Player("player", {
+let player;
+
+export function loadVideo(context, videoId) {
+  player = new YT.Player("player", {
     videoId: videoId,
-    width: `${playerWidth}`,
-    height: `${playerWidth * 9/16}`,
+    width: `600`,
+    height: `${600 * 9/16}`,
     events: {
-      "onReady": event => event.target.playVideo(),
+      "onReady": event => {
+        event.target.playVideo();
+        context.commit("showNewVideo", videoId);
+      },
       "onStateChange": event => {}
     }
   });
-  context.commit("showNewVideo", videoId);
+}
+
+export function destroyVideo(context) {
+  player.destroy();
+  context.commit("clearVideo");
 }
 
 export function showNewVideo(state, videoId) {
   state.currentVideo = {
     show: true,
     id: videoId
+  }
+}
+
+export function clearVideo(state) {
+  state.currentVideo = {
+    show: false,
+    id: null
   }
 }
