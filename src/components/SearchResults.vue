5de2f8dbcdbe988fd8aa296b04c894b03f53ff5a
cnsheafe <cnsheafe@gmail.com>
2017-10-26 12:19:25 -0600

    added result card

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -2,25 +2,31 @@
 <div v-show="!currentVideo.show" class="search-results">
   <ul>
     <li v-for="(result, index) in searchResults" :key="index">
-      <div class="result-left">
+      <result-card :result="result"></result-card>
+      <!-- <div class="result-left">
         <div class="result-header">
           <p @click="loadVideo" :data-index="index">{{result.title}}</p>
           <p>by {{result.channelName}}</p>
         </div>
-        <div class="result-description">
+        <div v-if="thumbnailSize > 0" class="result-description">
           <p>{{result.description}}</p>
         </div>
-        <div class="result-stats">
+        <div v-if="thumbnailSize > 0" class="result-stats">
           <div class="likes"><i class="fa fa-thumbs-up" aria-hidden="true"></i>{{result.likes}}</div>
           <div class="dislikes"><i class="fa fa-thumbs-down" aria-hidden="true"></i>{{result.dislikes}}</div>
-
-          <!-- <div class="views">{{result.views}}</div> -->
           <div class="duration"><i class="fa fa-clock-o" aria-hidden="true"></i>{{result.duration}}</div>
         </div>
       </div>
-      <div role="photo" class="search-result-right">
-        <img :src="result.thumbnails.small.url" :alt="result.title">
+      <div role="photo" class="result-right">
+        <img v-if="thumbnailSize < 2" :src="result.thumbnails.small.url" :alt="result.title">
+        <img v-if="thumbnailSize===2" :src="result.thumbnails.large.url" :alt="result.title">
       </div>
+      <div v-if="thumbnailSize === 0" class="result-stats">
+        <div class="likes"><i class="fa fa-thumbs-up" aria-hidden="true"></i>{{result.likes}}</div>
+        <div class="dislikes"><i class="fa fa-thumbs-down" aria-hidden="true"></i>{{result.dislikes}}</div>
+        <div class="duration"><i class="fa fa-clock-o" aria-hidden="true"></i>{{result.duration}}</div>
+      </div> -->
+
     </li>
   </ul>
 </div>
@@ -29,18 +35,43 @@
 <script>
 import store from "../store";
 import { mapState } from "vuex";
+import ResultCard from "./ResultCard";
 
 export default {
   name: "results",
-  computed: mapState(["searchResults", "currentVideo"]),
+  components: {
+    ResultCard: ResultCard
+  },
+  computed: mapState(["searchResults", "currentVideo", "thumbnailSize"]),
   methods: {
     loadVideo(event) {
       const id = event.target.dataset.index;
       const videoId = this.searchResults[event.target.dataset.index].id;
       store.dispatch("loadVideo", videoId);
     }
+  },
+  mounted: function() {
+    console.log("hello");
+    function mediaMatcher(e) {
+      let size;
+      if (e.matches) {
+        size = "small";
+      } else {
+        if (window.outerWidth < 992) {
+          size = "medium";
+        } else {
+          size = "large";
+        }
+      }
+      console.log(size);
+      store.dispatch("changeThumbnailSize", size);
+    }
+    const mql = window.matchMedia("(max-width: 768px)");
+
+    mediaMatcher(mql);
+    mql.onchange = mediaMatcher;
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -51,53 +82,42 @@ export default {
 }
 
 ul {
+  display: flex;
+  flex-wrap: wrap;
   padding-left: 0;
   list-style: none;
+  justify-content: space-around;
 }
 
 li {
   display: flex;
   flex-direction: row;
+  margin-right: 20px;
   margin-bottom: 20px;
-  justify-content: space-between;
+  justify-content: center;
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+}
+li:last-child {
 }
 
-p {
-  margin-top: 0;
-  margin-bottom: 0;
-}
+// p {
+//   margin-top: 0;
+//   margin-bottom: 0;
+// }
 
-.result-header {
-  display: flex;
-  flex-direction: column;
-  p {
-    margin-top: 0;
-    margin-bottom: 10px;
-  }
-  p:first-child {
-    font-size: 20px;
-    text-decoration: underline;
-    cursor: pointer;
-    display: inline-block;
-  }
-  p:last-child {
-    font-size: 16px;
-  }
-}
-.result-stats {
-  display: flex;
-  flex-direction: row;
-  margin-top: 10px;
-  div {
-    margin-right: 15px;
-  }
-  i {
-    margin-right: 5px;
-  }
-}
+// .result-left,
+// .result-right {
+//   display: flex;
+//   justify-content: center;
+// }
 
-.result-description {
-  width: 70%;
-}
+// .result-right {
+// }
 
+// .result-description {
+//   width: 70%;
+// }
 </style>
