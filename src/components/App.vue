0accabf06060bed2ea64c99e8ea3916391a1f5d2
cnsheafe <cnsheafe@gmail.com>
2017-11-03 12:37:15 -0600

    added nested routing
    Search results moved to sub-routes. Video player stays here because Youtube API needs the div #player to mount properly.

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,22 +1,20 @@
 <template>
     <section>
         <search-bar></search-bar>
-        <search-results></search-results>
-        <video-player></video-player>
+        <router-view/>
+        <video-player></video-player> 
     </section>
 </template>
 
 <script>
 import store from "../store";
 import SearchBar from "./SearchBar";
-import SearchResults from "./SearchResults";
 import VideoPlayer from "./VideoPlayer";
 
 export default {
   name: "app",
   components: {
     SearchBar: SearchBar,
-    SearchResults: SearchResults,
     VideoPlayer: VideoPlayer
   },
   store: store
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,27 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import App from "@/components/App";
+import SearchResults from "@/components/SearchResults";
+import VideoPlayer  from "@/components/VideoPlayer";
 
 Vue.use(Router);
 
 export default new Router({
-    routes: [{
-        path: '/',
-        name: 'App',
-        component: App
-    }],
-    mode: "history"
+  routes: [{
+      path: '/',
+      name: 'App',
+      component: App,
+      children: [
+        {
+        path: "results",
+        component: SearchResults, 
+        },
+        {
+          path: "video",
+          component: null
+        }
+      ]
+    },
+  ],
+  mode: "history"
 });
