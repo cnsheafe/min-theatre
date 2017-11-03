a5e78123f2d99fb46de838f7ae45f1a5a66210e6
cnsheafe <cnsheafe@gmail.com>
2017-11-03 14:54:48 -0600

    moved video player into own route

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,21 +1,18 @@
 <template>
     <section>
-        <search-bar></search-bar>
+        <search-bar/>
         <router-view/>
-        <video-player></video-player> 
     </section>
 </template>
 
 <script>
 import store from "../store";
 import SearchBar from "./SearchBar";
-import VideoPlayer from "./VideoPlayer";
 
 export default {
   name: "app",
   components: {
-    SearchBar: SearchBar,
-    VideoPlayer: VideoPlayer
+    SearchBar: SearchBar
   },
   store: store
 };
